7a9f4a718d2128739e7919df1e03e06d4909a47b
Konstantin Osipov <kostja@scylladb.com>
2020-01-17 21:01:47 +0300

    Use the interface which is only second to OCI in obscurity.
    Use it to start/stop instances.

--- actions.py
+++ actions.py
@@ -2,6 +2,7 @@
 from abc import abstractmethod
 import boto3
 import subprocess
+from pprint import pprint
 
 registry = {}
 
@@ -13,6 +14,39 @@ def register(name, description):
         registry[name] = Cls
         return Cls
     return decorator
+
+
+def ami_by_name(name):
+    client = boto3.client("ec2")
+    response = client.describe_images(Filters = [
+        {
+            "Name": "name",
+            "Values": [name],
+        }
+    ])
+    return response["Images"][0]["ImageId"]
+
+
+def instances_by_tag(tag):
+    client = boto3.client("ec2")
+    response = client.describe_instances(
+        Filters=
+        [
+            {
+                "Name": "tag:trawsers",
+                "Values": [ tag ]
+            },
+            {
+                "Name": "instance-state-name",
+                "Values": ["pending", "running", "shutting-down", "stopping", "stopped"]
+            },
+
+        ],
+    )
+    if len(response["Reservations"]) > 0:
+        return [instance["InstanceId"] for instance in response["Reservations"][0]["Instances"]]
+    else:
+        return []
 
 
 class Action:
@@ -58,5 +92,38 @@ class BuildAMI(Action):
 
 @register("boot", "Boot AMI instances")
 class BootInstances(Action):
+
     def func(self, args):
-        pass
+        instances = instances_by_tag("scylla")
+        if len(instances) == 0:
+            tag = {"Key": "trawsers", "Value": "scylla"}
+            ec2 = boto3.resource("ec2")
+            instances = ec2.create_instances(
+                ImageId=ami_by_name("trawsers-scylla"),
+                MinCount=3,
+                MaxCount=3,
+                InstanceType="i3.8xlarge",
+                TagSpecifications=[{'ResourceType': 'instance', 'Tags': [tag]}])
+            pprint(instances)
+        else:
+            client = boto3.client("ec2")
+            instances = client.start_instances(InstanceIds=instances)
+            pprint(instances)
+
+
+@register("halt", "Shutdown AMI instances")
+class ShutdownInstances(Action):
+
+    def func(self, args):
+        instances = instances_by_tag("scylla")
+        ec2 = boto3.resource("ec2")
+        response = ec2.instances.filter(InstanceIds=instances).stop()
+        pprint(response)
+
+@register("keys", "Show existing key pairs")
+class ShutdownInstances(Action):
+
+    def func(self, args):
+        client = boto3.client("ec2")
+        response = client.describe_key_pairs()
+        pprint(response)
