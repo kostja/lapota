2782e32208359ed6fcc980540e182e2560b9e097
Konstantin Osipov <kostja@scylladb.com>
2020-01-17 19:41:02 +0300

    Automate building AMIs.

--- actions.py
+++ actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import boto3
 from abc import abstractmethod
+import boto3
+import subprocess
 
 registry = {}
 
@@ -29,3 +30,33 @@ class ListInstances(Action):
         for instance in ec2.instances.all():
             print(instance.id, instance.state)
 
+
+@register("ami", "Build AMI instances for benchmark client and server")
+class BuildAMI(Action):
+    def func(self, args):
+        client = boto3.client("ec2")
+        response = client.describe_images(Filters = [
+            {
+                "Name": "name",
+                "Values": ["trawsers-*"],
+            }
+        ])
+        built_images = { image["Name"]: image for image in response["Images"] }
+
+        images = {
+            "trawsers-scylla": "resource/packer/scylla.json",
+            "trawsers-stress": "resource/packer/stress.json",
+        }
+
+        for key in images:
+            if key not in built_images:
+                subprocess.run(["packer", "build", images[key]])
+            else:
+                print("Reusing an existing image {} for {}".format(
+                    built_images[key]["ImageId"], key))
+
+
+@register("boot", "Boot AMI instances")
+class BootInstances(Action):
+    def func(self, args):
+        pass
